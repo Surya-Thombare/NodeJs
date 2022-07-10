dc611146a1c2ab4966ff95490da53b2c4dfaeaef
surya <surya@techsierra.in>
2022-07-10 21:27:11 +0530

    NodeJs

---
+++ config.js
@@ -0,0 +1,23 @@
+let environments = {}
+
+environments.satgging = {
+    'httpPort': 3000,
+    'httpsPort': 3001,
+    'envName': 'satgging'
+};
+
+environments.production = {
+    'httpPort': 5000,
+    'httpsPort': 5001,
+    'envName': 'production'
+};
+
+
+
+console.log(process.env.NODE_ENV);
+let currentEnvironment = typeof(process.env.NODE_ENV) == "string" ? process.env.NODE_ENV.toLowerCase() : '';
+console.log(currentEnvironment);
+
+let environmentsToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.satgging;
+
+module.exports = environmentsToExport;
